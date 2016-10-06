f5b240b5a052e2fd6a7cc99c4e8f9ffd10d94318
karatechops <contact@lazers.tv>
2016-10-06 13:25:53 -0400

    Fixed CaaS header bug.

--- src/js/components/ProgressBar.js
+++ src/js/components/ProgressBar.js
@@ -3,12 +3,8 @@ import React, { PropTypes } from 'react';
 const CLASS_ROOT = 'progress';
 
 export default function ProgressBar (props) {
-  const progress = (props.progress >= 2)
-    ? props.progress
-    : 0;
-
   const progressBar = (<div className={CLASS_ROOT} 
-    style={{width: `${progress}%`}} />);
+    style={{width: `${props.progress}%`}} />);
 
   return (
     <span className={`${CLASS_ROOT}__container`}>
